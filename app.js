96f8cd9dec2b2e581766aba5fbbb7429ac5e28d9
Thiago Guimarães <thiagoguimaraes96@outlook.com>
2022-10-13 20:40:43 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.dropMenu').addEventListener('click', function(){
+    document.querySelector('.submenu').classList.toggle('show')
+})
+document.querySelector('.dropLogin').addEventListener('click', ()=> { 
+    document.querySelector('.menu_Login').classList.toggle('show')
+})
